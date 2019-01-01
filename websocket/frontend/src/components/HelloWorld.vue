49420ca6eb767adbc7523ad2c42599d850c808e7
a63933 <1324147533@qq.com>
2019-01-01 18:32:29 +0800

    websocket with vue

--- websocket/frontend/src/components/HelloWorld.vue
+++ websocket/frontend/src/components/HelloWorld.vue
@@ -1,113 +1,87 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+<template lang="html">
+  <div class="">
+    <p>{{wsStatus}}</p>
+    <input type="button" ref="btn1" @click="createWs" name="" value="点击连接websocket">
+    <input type="button" ref="btn2" @click="closeWs" name="" value="点击断开websocket">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      oWs: null,
+      wsAddress: 'ws://localhost:3000',
+      wsStatus: '',
+      reconnectTimer: null,
+      heartbeatTimer: null,
+      isUserClose: false
+    }
+  },
+  created () {
+    setTimeout(() => {
+
+    }, 20)
+  },
+  methods: {
+    createWs () {
+      let oThis = this
+      if (oThis.oWs) { return }
+      oThis.isUserClose = false
+      if ('WebSocket' in window) {
+        oThis.oWs = new WebSocket(oThis.wsAddress)
+        console.log(oThis.oWs)
+
+        oThis.oWs.onopen = function () {
+          oThis.wsStatus = 'websocket已建立'
+          setTimeout(oThis.sendHeartBeat, 2000)
+        }
+
+        oThis.oWs.onerror = function () {
+          oThis.wsStatus = 'websocket发生错误'
+        }
+
+        oThis.oWs.onmessage = function (event) {
+          console.log('a')
+          oThis.wsStatus = `收到消息：${JSON.stringify(event.data)}`
+        }
+
+        oThis.oWs.onclose = function () {
+          if (oThis.isUserClose) {
+            oThis.wsStatus = 'websocket已关闭'
+          } else {
+            oThis.reconnectTimer = setTimeout(function () {
+              if (oThis.oWs && (oThis.oWs.readyState !== 1)) {
+                oThis.createWs()
+              } else {
+                clearTimeout(oThis.reconnectTimer)
+              }
+            }, 1000)
+          }
+        }
+      } else {
+        alert('Your browser do not support WebSocket, please use chrome.')
+      }
+    },
+    sendHeartBeat () {
+      const datahb = {type: 'heartbeat'}
+      this.oWs.send(JSON.stringify(datahb))
+      this.heartbeatTimer = setTimeout(this.sendHeartBeat, 1000)
+    },
+    closeWs () {
+      this.isUserClose = true
+      if (this.oWs) {
+        this.oWs.close()
+        clearTimeout(this.heartbeatTimer)
+        clearTimeout(this.reconnectTimer)
+      }
+      this.oWs = null
+      this.heartbeatTimer = null
+      this.reconnectTimer = null
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="css">
 </style>
--- websocket/frontend/src/main.js
+++ websocket/frontend/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
 import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
